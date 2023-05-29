03ff6d63f056e52c5df4410f65e274d5b63b07c9
denis.lischer <denislischer@hotmail.com>
2023-05-29 12:45:54 +0200

    Funktional Vote Button

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent {
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  votes: number;
+  title: string;
+  link: string;
+  
+  constructor() {
+  this.title = 'Angular';
+  this.link = 'http://angular.io';
+  this.votes = 10;
+  }
+  
+    voteUp(): boolean {
+  this.votes += 1;
+  return false;
+  }
+  
+  voteDown(): boolean {
+  this.votes -= 1;
+  return false;
+  }
 
+  ngOnInit() {
+  }
 }
